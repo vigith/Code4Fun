c69d209e3057b1b19277ce189fa1f5018a02b47c
Vigith Maurice <vigith@gmail.com>
2010-07-23 00:00:03 +0530

    Problem 31 (dynamic programming, added a soln i liked)

--- Project-Euler/p31.py
+++ Project-Euler/p31.py
@@ -43,3 +43,18 @@ if __name__ == '__main__':
                 ways[count] = 1
 
     print ways[200]
+
+
+# YET ANOTHER SOLUTION I LIKED (got from projecteuler thread)
+# total % c == 0 is good :-)
+# @Cached
+# def nway( total, coins):
+#     if not coins: return 0
+#     c, coins = coins[0], coins[1:]
+#     count = 0 
+#     if total % c == 0: count += 1    
+#     for amount in xrange( 0, total, c):
+#         count += nway(total - amount, coins)    
+#     return count
+# # main
+# print nway( 200, (1,2,5,10,20,50,100,200))
