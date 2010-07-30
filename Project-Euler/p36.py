f68655e42a9efe0d8cfd5253ff9b9205e3370e24
Vigith Maurice <vigith@gmail.com>
2010-07-30 15:32:00 +0530

    Problem 36 (add a logi ci liked)

--- Project-Euler/p36.py
+++ Project-Euler/p36.py
@@ -8,6 +8,23 @@
 # No even number's base 2 equivalent will have palindromic string. Check the number, if
 # it is a palindrome then check for base
 
+# GOOD LOGIC:
+# Also, for numbers under 1000000, palindromes are of one of these forms:
+# a
+# aa
+# aba
+# abba
+# abcba
+# abccba
+# A can be either 1, 3, 5, 7, or 9, while b and c can 0, 1, 2...8, 9. So the amount of numbers in each form are:
+# a - 5
+# aa - 5
+# aba - 50
+# abba - 50
+# abcba - 500
+# abccba - 500
+# This adds up to a total of 1110 possibilities for base 10 palindromes, instead of 999999. 
+
 def isPalindrome(array):
     length = None
     if len(array) % 2 == 0: length = len(array) / 2
